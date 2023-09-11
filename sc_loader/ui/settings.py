e085554c30a4147ef7abeeff73527f4fcc3e3a77
Neomyte Diquama <neomyte@gmail.com>
2023-09-11 18:25:44 +0200

    :recycle: Clean up

--- sc_loader/ui/settings.py
+++ sc_loader/ui/settings.py
@@ -1,17 +1,22 @@
+import os
+
 import gradio as gr
 
-from modules import shared
+from modules import shared, scripts
 
 class SettingsTab:
     def build(self):
-        section = ('sc_loader', 'Scenario loader')
-        shared.opts.add_option(
-            'sc_loader_config_path',
-            shared.OptionInfo(
-                'extensions/sd-webui-sc-loader/base_configs',
-                'Path to configuration',
-                gr.Textbox,
-                {'interactive': True},
-                section=section
+        default_path = os.path.join(scripts.current_basedir, 'base_configs')
+        shared.options_templates.update(
+            shared.options_section(
+                ('sc_loader', 'Scenario loader'),
+                {
+                    'sc_loader_config_path': shared.OptionInfo(
+                        default_path,
+                        'Custom path to configuration',
+                        gr.Textbox,
+                        {'interactive': True}
+                    ).info(f'Defaults to "{default_path}"'),
+                }
             )
         )
